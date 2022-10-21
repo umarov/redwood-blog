bc69e8aea4e295c42fd6c54d2d38556afe78e58b
Muzafar Umarov <mumarov@invoca.com>
2022-10-21 17:18:26 -0400

    Initial commit

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+      <p>Welcome to the blog!</p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
